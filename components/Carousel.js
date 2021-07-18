a9682fff4ec381e96b174a1ca37db9d32bd0da3c
Anibal Santos <anibalsantosgo@gmail.com>
2021-07-18 04:14:19 +0200

    mobile

--- components/Carousel.js
+++ components/Carousel.js
@@ -9,10 +9,19 @@ const Carousel = ({ data }) => {
   return (
     <>
       <Link key={slug} href={`/stories/${slug}`} className="pt-20 pb-10 hidden lg:block">
-        <Flicking align="prev" interruptable={false}>
+        <Flicking align="prev" interruptable={false} className="rounded-md">
           {images.map((panel, index) => (
-            <div key={index} className="mx-4">
-              <Image className="rounded-md" alt="" src={panel} width={700} height={480} />
+            <div key={index} className="px-4">
+              <Image className="rounded-md" alt="" src={panel} width={768} height={476} />
+            </div>
+          ))}
+        </Flicking>
+      </Link>
+      <Link key={slug} href={`/stories/${slug}`} className="lg:hidden">
+        <Flicking align="prev" interruptable={false} className="rounded-md">
+          {images.map((panel, index) => (
+            <div key={index} className="px-4">
+              <Image className="rounded-md" alt="" src={panel} width={368} height={176} />
             </div>
           ))}
         </Flicking>
