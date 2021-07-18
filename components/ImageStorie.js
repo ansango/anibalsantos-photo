b4a5e0a43ccac5cb0abea5fa17c910ce10b6ef37
Anibal Santos <anibalsantosgo@gmail.com>
2021-07-18 23:00:55 +0200

    gallery ready

--- components/ImageStorie.js
+++ components/ImageStorie.js
@@ -4,38 +4,27 @@ import Modal from './Modal'
 
 const ImageStorie = ({ gallery }) => {
   const [showModal, setShowModal] = useState(false)
-  const [data, setData] = useState({})
-  const [src, setSrc] = useState('')
+  const [srcImg, setSrcImg] = useState('')
   const onCloseHandler = (event) => {
     setShowModal(false)
-    setData({})
   }
-  const onOpenHandler = () => {
+  const onOpenHandler = (src) => {
+    setSrcImg(src)
     setShowModal(true)
-    console.log(src)
-    setData({ image: src })
-  }
-  const onEnterKey = (event) => {
-    if (event.key === 'Enter') {
-      onOpenHandler()
-    }
   }
   return (
     <>
-      {showModal && <Modal onClose={onCloseHandler} data={data} />}
+      {showModal && <Modal onClose={onCloseHandler} onImg={srcImg} />}
       <div className="grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-7 lg:grid-cols-3">
         {gallery.map((src, index) => {
           return (
-            <div
-              className="cursor-pointer md:transform md:transition md:duration-500 md:hover:scale-105"
+            <button
+              className="md:transform md:transition md:duration-500 md:hover:scale-105"
               key={index}
-              onClick={onOpenHandler}
-              onKeyPress={onEnterKey}
-              role="button"
-              tabIndex="0"
+              onClick={() => onOpenHandler(src)}
             >
               <Image className="rounded-md" src={src} alt="" width={768} height={473} />
-            </div>
+            </button>
           )
         })}
       </div>
