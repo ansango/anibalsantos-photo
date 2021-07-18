adf30f9a2f802132028ffd6183c5c773b8d9e503
Anibal Santos <anibalsantosgo@gmail.com>
2021-07-18 07:37:58 +0200

    home

--- components/Carousel.js
+++ components/Carousel.js
@@ -18,15 +18,15 @@ const Carousel = ({ data }) => {
     <Swiper
       className="w-full h-full"
       slidesPerView={2}
-      spaceBetween={40}
+      spaceBetween={10}
       width={320}
       breakpoints={{
-        576: { width: 768 },
-        768: { width: 992, slidesPerView: 3 },
-        992: { width: 1024, slidesPerView: 3 },
-        1024: { width: 1200, slidesPerView: 3 },
-        1200: { width: 1600, slidesPerView: 3 },
-        1600: { width: 1920, slidesPerView: 3 },
+        576: { width: 768, spaceBetween: 30 },
+        768: { width: 992, slidesPerView: 3, spaceBetween: 40 },
+        992: { width: 1024, slidesPerView: 3, spaceBetween: 40 },
+        1024: { width: 1200, slidesPerView: 3, spaceBetween: 40 },
+        1200: { width: 1600, slidesPerView: 3, spaceBetween: 40 },
+        1600: { width: 1920, slidesPerView: 3, spaceBetween: 40 },
       }}
     >
       {images.map((panel, index) => (
