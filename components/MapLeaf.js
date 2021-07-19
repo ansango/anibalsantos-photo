a19b8b1fade03d0f8f3bc0609f7ae21dcd15e6dc
Anibal Santos <anibalsantosgo@gmail.com>
2021-07-19 02:43:23 +0200

    stories

--- components/MapLeaf.js
+++ components/MapLeaf.js
@@ -2,12 +2,12 @@ import { MapContainer, TileLayer, CircleMarker, Tooltip } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
 import 'leaflet-defaulticon-compatibility'
-const position = [42.2406, -8.7207]
+const position = [46.2406, 8.7207]
 const MapLeaf = ({ data }) => {
   const locations = data.flat()
   return (
     <div className="p-5 mx-auto container">
-      <MapContainer center={position} zoom={7} scrollWheelZoom={false} className="map-leaf">
+      <MapContainer center={position} zoom={5} scrollWheelZoom={true} className="map-leaf">
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
