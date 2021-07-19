f26a26fbfa5f92d18d67e7188a910c32e00a1b99
Anibal Santos <anibalsantosgo@gmail.com>
2021-07-19 02:49:08 +0200

    modal

--- components/Modal.js
+++ components/Modal.js
@@ -6,7 +6,7 @@ const Modal = ({ onClose, onImg }) => {
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none px-5">
         <div className="w-auto my-6 mx-auto max-w-7xl">
           {/*content*/}
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white dark:bg-gray-900 outline-none focus:outline-none">
             {/*body*/}
             <div className="relative p-6 flex-auto">
               <Image
