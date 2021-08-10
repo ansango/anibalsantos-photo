bb274d77e8da4074e8ec273ca6b226222970cc27
ansango <anibalsantosgo@gmail.com>
2021-08-10 19:02:40 +0200

    places

--- layouts/PlacesLayout.js
+++ layouts/PlacesLayout.js
@@ -1,14 +1,15 @@
 import Places from '@/components/Places'
+import removeDuplicates from '@/lib/utils/removeDuplicates'
 
 export default function PlacesLayout({ posts }) {
-  const locations = posts
+  const AllLocationsPosts = posts
     .map((post) => {
       return post.location.map((loc) => {
         return { slug: post.slug, ...loc }
       })
     })
     .flat()
-
+  const locations = removeDuplicates(AllLocationsPosts, (location) => location.name)
   const mapSettings = { coordinates: locations, center: [43.0, 3.0], zoom: 4 }
 
   return (
