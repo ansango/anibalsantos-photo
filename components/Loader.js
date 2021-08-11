00558e3de4ad1d412ab7b7a8c0d742828ff6838a
ansango <anibalsantosgo@gmail.com>
2021-08-11 13:18:59 +0200

    grid

--- components/Loader.js
+++ components/Loader.js
@@ -15,7 +15,7 @@ export const TitleLoader = () => {
 
 export const CardLoader = () => {
   return (
-    <div className="border border-primary-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+    <div className="border border-primary-300 shadow rounded-md p-4 w-full mx-auto">
       <div className="animate-pulse flex space-x-4">
         <div className="flex-1 space-y-4 py-1">
           <div className="h-4 bg-primary-400 rounded w-3/4"></div>
--- layouts/PlacesLayout.js
+++ layouts/PlacesLayout.js
@@ -30,33 +30,26 @@ export default function PlacesLayout({ posts }) {
     <div className="px-5">
       <PageTitle>{'All places where I shooted'}</PageTitle>
       <div>Locations: {data.locations}</div>
-      <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
-        <div className="xl:col-span-2">
+      <div className="grid grid-cols-1 gap-5">
+        <div className="">
           <Places mapSettings={mapSettings} onLocationSelected={addPlaceHandler} viewPage={true} />
         </div>
 
         {place && !isLoading && (
-          <div>
-            <div>
-              <h3 className="font-bold text-3xl md:text-4xl py-6 md:py-8 text-center">
-                {place}
-                <span className="text-2xl md:text-3xl" role="img" aria-label="">
-                  📍
-                </span>
-              </h3>
-              <div className="border-dashed border-4 border-primary-200 rounded-md">
-                <div className="px-10"></div>
-              </div>
-            </div>
-          </div>
+          <h3 className="font-bold text-3xl md:text-4xl py-6 md:py-8 text-center">
+            {place}
+            <span className="text-2xl md:text-3xl" role="img" aria-label="">
+              📍
+            </span>
+          </h3>
         )}
 
         {isLoading ? (
           <>
-            <div>
+            <div className="max-w-xl w-full mx-auto">
               <FillLoader amount={1} Component={TitleLoader} />
             </div>
-            <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 lg:grid-cols-3 xl:grid-cols-2 2xl:grid-cols-3">
+            <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4">
               <FillLoader amount={6} Component={CardLoader} />
             </div>
           </>
