b4480777dba741a362eb42245a3e11cad46acfa3
ansango <anibalsantosgo@gmail.com>
2021-08-11 20:25:46 +0200

    publish

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/stories', title: 'Stories' },
   //{ href: '/tags', title: 'Tags' },
+  { href: '/places', title: 'Places' },
   { href: '/about', title: 'About' },
 ]
 
