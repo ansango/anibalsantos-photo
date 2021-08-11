a6dd46ab833512fb2e0cca3825510278c198c25d
ansango <anibalsantosgo@gmail.com>
2021-08-11 13:19:56 +0200

    clean

--- components/PlacesGallery.js
+++ components/PlacesGallery.js
@@ -6,27 +6,16 @@ const PlacesGallery = ({ galleryMap, place }) => {
   const gallery = data.map((item) => item.img).flat()
 
   return (
-    <>
-      <div className="">
-        <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4">
-          {gallery.map((src, index) => {
-            return (
-              <div key={index} className="relative">
-                <Image
-                  className="rounded-md"
-                  src={src}
-                  alt=""
-                  width={680}
-                  height={420}
-                  quality={33}
-                />
-                <ExpandIcon size={25} className="absolute right-5 bottom-5 text-primary-100" />
-              </div>
-            )
-          })}
-        </div>
-      </div>
-    </>
+    <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4">
+      {gallery.map((src, index) => {
+        return (
+          <div key={index} className="relative">
+            <Image className="rounded-md" src={src} alt="" width={680} height={420} quality={33} />
+            <ExpandIcon size={25} className="absolute right-5 bottom-5 text-primary-100" />
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
