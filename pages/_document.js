f9b30399c38d0da0c7e682458c658c534b62f600
ansango <anibalsantosgo@gmail.com>
2021-09-09 18:55:02 +0200

    katex remove

--- pages/_document.js
+++ pages/_document.js
@@ -27,12 +27,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css"
-            integrity="sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc"
-            crossOrigin="anonymous"
-          />
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script async src={`https://www.googletagmanager.com/gtag/js?id=G-RWYBL43Q0E`} />
           <script
