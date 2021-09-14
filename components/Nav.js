4d307016e084fb05bf6c2183031baf8a022d6eb7
ansango <anibalsantosgo@gmail.com>
2021-09-14 20:25:17 +0200

    nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,23 +1,29 @@
 import headerNavLinks from '@/data/headerNavLinks'
+import { useRouter } from 'next/router'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 
 const Nav = () => {
+  const { pathname: route } = useRouter()
   return (
     <header className="flex items-center justify-between py-10">
       <div></div>
       <div className="flex items-center text-base leading-5">
         <div className="hidden sm:block">
-          {headerNavLinks.map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100"
-            >
-              {link.title}
-            </Link>
-          ))}
+          {headerNavLinks.map((link) => {
+            if (link.href !== route) {
+              return (
+                <Link
+                  key={link.title}
+                  href={link.href}
+                  className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100"
+                >
+                  {link.title}
+                </Link>
+              )
+            }
+          })}
         </div>
         <ThemeSwitch />
         <MobileNav />
